6ec7b783113bf3e7ea2c1893adb98325a92b33d6
Harsh Saini <harshsaini697@gmail.com>
2021-02-23 20:02:45 -0600

    54 ms

---
+++ solutions/1451. Rearrange Words in a Sentence.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String arrangeWords(String text) {
+        //base
+        if(text == null || text.length() == 0) return text;
+        text = text.toLowerCase();
+        String[] texts = text.split(" ");
+        
+        //Main Fn Logic
+        Arrays.sort(texts, (a, b) -> a.length() - b.length());
+        
+        // Modifying First Char to Upper Case
+        char[] arr = texts[0].toCharArray();
+        arr[0] = Character.toUpperCase(arr[0]);
+        texts[0] = String.valueOf(arr);
+        
+        //Result
+        return String.join(" ", texts);
+    }
+}
+​
+//Leetcode is cool
+​
+//leetCode,is,cool
