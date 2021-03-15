ab20fa1c336b3cda2b4f882ae3383b7c9c9ce5f4
Harsh Saini <harshsaini697@gmail.com>
2021-03-15 13:12:52 -0500

    3 ms

--- solutions/938. Range Sum of BST.java
+++ solutions/938. Range Sum of BST.java
@@ -15,10 +15,33 @@
  */
 class Solution {
     public int rangeSumBST(TreeNode root, int low, int high) {
-        if(root == null) return 0;
-        if(root.val < low) return rangeSumBST(root.right, low, high);
-        if(root.val > high) return rangeSumBST(root.left, low, high);
-        return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+        // if(root == null) return 0;
+        // if(root.val < low) return rangeSumBST(root.right, low, high);
+        // if(root.val > high) return rangeSumBST(root.left, low, high);
+        // return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+        return iterative(root, low, high);
+    }
+    private int iterative(TreeNode node, int low, int high){
+        if(node == null) return 0;
+        TreeNode root = node;
+        int sum = 0;
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        while( !st.isEmpty()){
+            root = st.pop();
+            if (root == null) { continue; }
+            if(root.val >= low && root.val <= high){
+                sum += root.val;
+            }
+            
+            if(root.val > low){
+                st.push(root.left);
+            }
+            if(root.val < high){
+                st.push(root.right);
+            }
+        }
+        return sum;
     }
     
 }
