0d9a3691ba18c281341562257d3168f950b0ac24
Harsh Saini <harshsaini697@gmail.com>
2021-03-17 23:09:56 -0500

    23 ms

--- solutions/5. Longest Palindromic Substring.java
+++ solutions/5. Longest Palindromic Substring.java
@@ -1,25 +1,29 @@
 class Solution {
+    int max = 1;
+    int start = 0 ;
     public String longestPalindrome(String s) {
         if(s == null || s.length() == 0) return "";
         int n = s.length();
         if(n < 2) return s;
-        boolean[][] dp = new boolean[n][n];
-        int start = 0;
-        int end = 0;
-        int max = 1;
         
         for(int i = 0; i < n; i++){
-            for(int j = 0; j <= i; j++){
-                if(s.charAt(i) == s.charAt(j) && ( i - j < 2 || dp[i-1][j+1])){
-                    dp[i][j] = true;
-                    if(max < i- j + 1){
-                        start = j;
-                        end = i;
-                        max = i - j + 1;
-                    }
-                }
-            }
+            expandAroundCenter(s, i, i);
+            if(i < n - 1 &&s.charAt(i) == s.charAt(i+1)) 
+                expandAroundCenter(s, i, i + 1); // even case
+            
         }
-        return s.substring(start, end + 1);
+        return s.substring(start, start + max);
+    }
+    private void expandAroundCenter(String s, int left, int right){
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        if(R - L - 1 > max){
+            max = R - L - 1;
+            start = L + 1;
+        }
+​
     }
 }
