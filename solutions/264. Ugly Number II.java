a1cf0e84c40f8284538aa274b7ac64ad288a9378
Harsh Saini <harshsaini697@gmail.com>
2021-03-18 22:22:44 -0500

    2 ms

--- solutions/264. Ugly Number II.java
+++ solutions/264. Ugly Number II.java
@@ -1,14 +1,28 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        if(n==1) return 1;
-        TreeSet<Long> q = new TreeSet<>();
-        q.add(1l);
-        for(int i = 0; i < n - 1; i++){
-            long poll = q.pollFirst();
-            q.add(poll * 2);
-            q.add(poll * 3);
-            q.add(poll * 5);
+        int p1 = 0;
+        int p2 = 0;
+        int p3 = 0;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        for(int i = 1; i < n; i++){
+            int n2 = dp[p1] * 2;// 2
+            int n3 = dp[p2] * 3;//3
+            int n5 = dp[p3] * 5;//5
+            int min = Math.min(n2, Math.min(n3, n5));//2
+            if(n2 == min){
+                dp[i] = n2;
+                p1++;
+            }
+            if(n3 == min){
+                dp[i] = n3;
+                p2++;
+            }
+            if(n5 == min){
+                dp[i] = n5;
+                p3++;
+            }
         }
-        return q.first().intValue();
+        return dp[n - 1];
     }
 }
