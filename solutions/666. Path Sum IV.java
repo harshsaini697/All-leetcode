00218f7663f69387722df319f86e8042c2e15c0b
Harsh Saini <harshsaini697@gmail.com>
2021-03-19 20:11:50 -0500

    1 ms

--- solutions/666. Path Sum IV.java
+++ solutions/666. Path Sum IV.java
@@ -1,5 +1,28 @@
 class Solution {
+    int ans = 0;
+    Map<Integer, Integer> map;
     public int pathSum(int[] nums) {
-        
+        map = new HashMap<>();
+        for(int num: nums){
+            map.put(num / 10, num % 10);
+        }
+        dfs(nums[0] / 10, 0);
+        return ans;
+    }
+    private void dfs(int node, int sum){
+        if(!map.containsKey(node)){
+            return;
+        }
+        sum += map.get(node);
+        int depth = node /10; 
+        int pos = node % 10;
+        int left = (depth + 1) * 10 + 2 * pos - 1;
+        int right = left + 1;
+        if(!map.containsKey(left) && !map.containsKey(right)){
+            ans += sum;
+        }else{
+            dfs(left, sum);
+            dfs(right, sum);
+        }
     }
 }
