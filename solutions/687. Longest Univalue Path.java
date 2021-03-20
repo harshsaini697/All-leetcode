ef26a8ac8d5621bb00ef9f5eb5ccfc486d68c95b
Harsh Saini <harshsaini697@gmail.com>
2021-03-20 04:39:45 -0500

    2 ms

---
+++ solutions/687. Longest Univalue Path.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int len = 0;
+    public int longestUnivaluePath(TreeNode root) {
+        if(root == null) return len;
+        postorder(root, root.val);
+        return len;
+    }
+    private int postorder(TreeNode root, int value){
+        if(root == null) return 0;
+        int left = postorder(root.left, root.val);
+        int right = postorder(root.right, root.val);
+        len = Math.max(len, left + right);
+        if(value == root.val) return Math.max(left, right) + 1;
+        return 0;
+    }
+}
