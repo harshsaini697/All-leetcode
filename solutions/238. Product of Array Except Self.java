223d266d018509e488854ac8a7cc75ac9b3ff1d4
Harsh Saini <harshsaini697@gmail.com>
2021-03-24 02:14:08 -0500

    1 ms

--- solutions/238. Product of Array Except Self.java
+++ solutions/238. Product of Array Except Self.java
@@ -3,18 +3,19 @@ class Solution {
         if(nums == null || nums.length == 0) return new int[]{};
         int[] result = new int[nums.length];
         int[] L = new int[nums.length];
-        int[] R = new int[nums.length];
-        L[0] = 1;
+        // int[] R = new int[nums.length];
+        result[0] = 1;
         for(int i = 1; i < nums.length; i++){
-            L[i] = nums[i - 1] * L[i - 1];
+            result[i] = nums[i - 1] * result[i - 1];
         }
-        R[nums.length - 1] = 1;
-        for(int i = nums.length - 2; i >=0 ; i--){
-            R[i] = nums[i + 1] * R[i + 1];
+        int R = 1;
+        for(int i = nums.length - 1; i >=0 ; i--){
+            result[i] = result[i] * R;
+            R *= nums[i];
         }
-        for(int i = 0; i < nums.length; i++){
-            result[i] = L[i] * R[i];
-        }
+        // for(int i = 0; i < nums.length; i++){
+        //     result[i] = L[i] * R[i];
+        // }
         return result;
     }
 }
