f2a914b953feaf36cabd0dd63017a0d8543ea885
Harsh Saini <harshsaini697@gmail.com>
2021-04-02 02:16:39 -0500

    28 ms

--- solutions/239. Sliding Window Maximum.java
+++ solutions/239. Sliding Window Maximum.java
@@ -1,24 +1,21 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        if (nums == null || nums.length == 0) {
-            return nums;
-        }
+        if(nums == null || nums.length == 0) return new int[]{};
         int n = nums.length;
-        
-        int[] result = new int[n - k + 1];
+        int[] res = new int[n - k + 1];
         LinkedList<Integer> dq = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            if (!dq.isEmpty() && dq.peek() < i - k + 1) {
+        for(int i = 0; i < n; i++){
+            if(!dq.isEmpty() && dq.peek() < i - k + 1){
                 dq.poll();
             }
-            while (!dq.isEmpty() && nums[i] >= nums[dq.peekLast()]) {
+            while(!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]){
                 dq.pollLast();
             }
             dq.offer(i);
-            if (i - k + 1 >= 0) {
-                result[i - k + 1] = nums[dq.peek()];
+            if(i - k + 1 >= 0){
+                res[i - k + 1] = nums[dq.peek()];                
             }
         }
-        return result;
+        return res;
     }
 }
