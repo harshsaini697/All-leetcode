4a20d29ca43070b572fffe1ca05671c54d637302
Harsh Saini <harshsaini697@gmail.com>
2021-04-03 12:36:16 -0500

    2 ms

---
+++ solutions/32. Longest Valid Parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        int maxans = 0;
+        Stack<Integer> st = new Stack<>();
+        st.push(-1);
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '('){
+                st.push(i);
+            }else{
+                st.pop();
+                if(st.isEmpty()){
+                    st.push(i);
+                }else{
+                    maxans = Math.max(maxans, i - st.peek());
+                }
+            }
+        }
+        return maxans; 
+    }
+}
