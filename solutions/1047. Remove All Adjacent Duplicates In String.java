0261c54bfbb82f04486c49c68fb58253babed056
Harsh Saini <harshsaini697@gmail.com>
2021-04-15 17:28:16 -0500

    8 ms

---
+++ solutions/1047. Remove All Adjacent Duplicates In String.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String removeDuplicates(String S) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < S.length(); i++){
+            int size = sb.length();
+            if(size > 0 && sb.charAt(size - 1) == S.charAt(i)){
+                sb.deleteCharAt(size - 1);
+            }else{
+                sb.append(S.charAt(i));
+            }    
+        }
+        return sb.toString();
+    }
+}
