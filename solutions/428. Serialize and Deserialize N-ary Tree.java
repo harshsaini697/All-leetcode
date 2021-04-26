ca4c69291bba4cfabc694c6ce3db2d9cb1e34805
Harsh Saini <harshsaini697@gmail.com>
2021-04-25 21:32:56 -0500

    10 ms

--- solutions/428. Serialize and Deserialize N-ary Tree.java
+++ solutions/428. Serialize and Deserialize N-ary Tree.java
@@ -1,13 +1,3 @@
-*/
-​
-class Codec {
-    // Encodes a tree to a single string.
-    public String serialize(Node root) {
-        if(root == null) return "";
-        StringBuilder sb = new StringBuilder();
-        Queue<Node> q = new LinkedList();
-        q.offer(root);
-        sb.append(root.val).append(",").append(root.children.size()).append(",");
         while(!q.isEmpty()){
             Node curr = q.poll();
             for(Node c: curr.children){
@@ -25,21 +15,22 @@ class Codec {
             return null;
         }
         String[] nodes = data.split(",");
-        Queue<Node> queue = new LinkedList<Node>();
-        Queue<Integer> childQueue = new LinkedList<Integer>();
+        Queue<Node> q = new LinkedList<>();
+        Queue<Integer> childq = new LinkedList<>();
         Node root = new Node(Integer.valueOf(nodes[0]));
-        queue.offer(root);
-        childQueue.offer(Integer.valueOf(nodes[1]));
+        q.add(root);
+        childq.add(Integer.valueOf(nodes[1]));
         int i = 2;
-        while (!queue.isEmpty()) {
-            Node cur = queue.poll();
-            cur.children = new ArrayList<>();
-            int n = childQueue.poll();
-            for (int j = 0; j < n; j++) {
+        while(!q.isEmpty()){
+            Node curr = q.poll();
+            curr.children = new ArrayList<>();
+            int size = childq.poll();
+            for(int j = 0; j < size; j++){
                 Node child = new Node(Integer.valueOf(nodes[i++]));
-                childQueue.offer(Integer.valueOf(nodes[i++]));
-                queue.offer(child);
-                cur.children.add(child);
+                int children = Integer.valueOf(nodes[i++]);
+                q.add(child);
+                childq.add(children);
+                curr.children.add(child);
             }
         }
         return root;
