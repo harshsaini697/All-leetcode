adfd7b521c44892e50b5c55111d5935e92d9fa6b
Harsh Saini <harshsaini697@gmail.com>
2021-04-28 00:26:42 -0500

    1 ms

--- solutions/42. Trapping Rain Water.java
+++ solutions/42. Trapping Rain Water.java
@@ -1,27 +1,20 @@
 class Solution {
     public int trap(int[] height) {
-        if(height == null || height.length == 0) return 0;
-        int ans = 0;
-        int[] left = new int[height.length];
-        int[] right = new int[height.length];
-        left[0] = height[0];
-        for(int i = 1; i < height.length; i++){
-            left[i] = Math.max(left[i-1], height[i]);
-            
+        if (height.length==0) return 0; 
+        int left = 0, right = height.length-1; 
+        int leftMax=0, rightMax=0; 
+        int ans = 0; 
+        while(left < right){
+            if(height[left] >= leftMax) leftMax = height[left];
+            if(height[right] >= rightMax) rightMax = height[right];
+            if(leftMax < rightMax){
+                ans += leftMax - height[left];
+                left++;
+            }else{
+                ans += rightMax - height[right];
+                right--;
+            }
         }
-        //System.out.println(Arrays.toString(left));
-        right[height.length - 1] = height[height.length - 1];
-        for(int j = height.length - 2; j >=0; j--){
-            right[j] = Math.max(right[j+1], height[j]);
-            
-        }
-        //System.out.println(Arrays.toString(right));
-        for(int i = 0; i < height.length - 1; i++){
-            ans += Math.min(right[i], left[i]) - height[i];
-            //System.out.println(ans);
-        }
-        
-        
-        return ans;
+        return ans; 
     }
 }
