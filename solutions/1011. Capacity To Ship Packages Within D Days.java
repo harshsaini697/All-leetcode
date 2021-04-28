ada2e9d28e2f217750b420736655f40b7195f891
Harsh Saini <harshsaini697@gmail.com>
2021-04-28 13:53:58 -0500

    8 ms

--- solutions/1011. Capacity To Ship Packages Within D Days.java
+++ solutions/1011. Capacity To Ship Packages Within D Days.java
@@ -1,27 +1,31 @@
 class Solution {
     public int shipWithinDays(int[] weights, int D) {
-        if(weights == null || weights.length == 0) return 0;
-        int min = 0;
+        if(weights == null || weights.length == 0){
+            return 0;
+        }
+        int n = weights.length;
         int max = 0;
-        for(int i = 0; i < weights.length; i++){
-            min = Math.max(min, weights[i]); //min capacity that we need
+        int min = 0;
+        for(int i = 0; i < n; i++){
+            min = Math.max(min, weights[i]);
             max += weights[i];
         }
+        
         while(min <= max){
+            int count = 1;
             int mid = min + (max - min) / 2;
             int currMax = 0;
-            int count = 1;
-            for(int i = 0; i < weights.length; i++){
+            for(int i = 0; i < n; i++){                
                 if(currMax + weights[i] > mid){
                     count++;
                     currMax = 0;
                 }
                 currMax += weights[i];
             }
-            if(count > D){
-                min = mid + 1;
-            }else{
+            if(count <= D){
                 max = mid - 1;
+            }else{
+                min = mid + 1;
             }
         }
         return min;
