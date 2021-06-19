32e2d51c19aaf5f9fad7748ab2c4bc04eec3bd5d
Harsh Saini <harshsaini697@gmail.com>
2021-06-19 00:02:43 -0500

    0 ms

--- solutions/589. N-ary Tree Preorder Traversal.java
+++ solutions/589. N-ary Tree Preorder Traversal.java
@@ -18,19 +18,29 @@ class Node {
 */
 ​
 class Solution {
+    List<Integer> res;
     public List<Integer> preorder(Node root) {
-        List<Integer> res = new ArrayList<>();
+        res = new ArrayList<>();
         if(root == null) return res;
-        Stack<Node> st = new Stack<>();
-        st.push(root);
+//         Stack<Node> st = new Stack<>();
+//         st.push(root);
         
-        while(!st.isEmpty()){
-            root = st.pop();
-            res.add(root.val);
-            for(int k = root.children.size() - 1; k >= 0; k--){ //adding from back to make sure reverse order turns out
-                st.push(root.children.get(k));
-            } 
-        }
+//         while(!st.isEmpty()){
+//             root = st.pop();
+//             res.add(root.val);
+//             for(int k = root.children.size() - 1; k >= 0; k--){ //adding from back to make sure reverse order turns out
+//                 st.push(root.children.get(k));
+//             } 
+//         }
+        pre(root);
         return res;
+    }
+    
+    private void pre(Node root){
+        if(root == null) return;
+        res.add(root.val);
+        for(int k = 0; k < root.children.size() ; k++){
+            pre(root.children.get(k));
+        }
     }
 }
