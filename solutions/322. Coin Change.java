94bb5297ea9f0742f0bf63f187ba577c5d7b8537
Harsh Saini <harshsaini697@gmail.com>
2021-07-08 21:36:54 -0500

    17 ms

--- solutions/322. Coin Change.java
+++ solutions/322. Coin Change.java
@@ -1,23 +1,25 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if(coins == null || coins.length == 0) return 0;
-        int[][] dp = new int[coins.length + 1][amount + 1];
-        dp[0][0] = 0;
-        for(int i = 1; i <= amount; i++){
-            dp[0][i] = amount + 1;
-        }
-        for(int i = 1; i <= coins.length; i++){
-            for(int j = 1; j <= amount; j++){
-                if(coins[i - 1] <= j){
-                    dp[i][j] = Math.min(dp[i - 1][j], 1 + dp[i][j - coins[i - 1]]);
-                }else{
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
+        if(coins == null|| coins.length == 0) return 0;
+        return recurse(coins, amount, 0, 0);
+    }
+    private int recurse(int[] coins, int amount, int index, int coin){
+        //base
+        if(amount == 0){
+            return coin;
         }
-        if(dp[coins.length][amount] >= amount + 1){
+        if(index >= coins.length || amount < 0){
             return -1;
         }
-        return dp[coins.length ][amount];
+        
+        //logic
+        //choose
+        int case1 = recurse(coins, amount - coins[index], index, coin + 1);
+        //notchosoe
+        int case2 = recurse(coins, amount , index + 1, coin);
+        if(case1== -1) return case2;
+        if(case2== -1) return case1;
+        
+        return Math.min(case1, case2);
     }
 }
