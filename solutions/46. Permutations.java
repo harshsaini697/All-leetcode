86fbe16580b3e86fd68647401f7bc982c2ff0f44
Harsh Saini <harshsaini697@gmail.com>
2021-09-06 22:02:45 -0500

    1 ms

--- solutions/46. Permutations.java
+++ solutions/46. Permutations.java
@@ -2,18 +2,18 @@ class Solution {
     List<List<Integer>> res;
     public List<List<Integer>> permute(int[] nums) {
         res = new ArrayList<>();
-        backtrack(nums, new ArrayList<>(), 0);
+        backtrack(nums, new ArrayList<>());
         return res;
         
     }
-    private void backtrack(int[] nums, List<Integer> temp, int start){
+    private void backtrack(int[] nums, List<Integer> temp){
         if(temp.size() == nums.length) 
             res.add(new ArrayList<>(temp));
         else{
             for(int i = 0; i < nums.length; i++){
                 if(temp.contains(nums[i])) continue;
                 temp.add(nums[i]);
-                backtrack(nums, temp, i+1);
+                backtrack(nums, temp);
                 temp.remove(temp.size() - 1);
             }
         }
