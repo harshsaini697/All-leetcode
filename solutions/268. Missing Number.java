6e07cd29afaf5c1a5e6e36cc5eb92aada9f3eddb
Harsh Saini <harshsaini697@gmail.com>
2022-02-12 16:18:40 -0600

    0 ms

---
+++ solutions/268. Missing Number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int result = (n * (n + 1))/2;
+        
+        for(int num : nums) {
+            result -= num;
+        }
+        return result;
+    }
+}
