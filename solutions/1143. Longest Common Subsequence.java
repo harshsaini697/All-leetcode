91a6766af7f8ba252ed57b9099d8a085d9d9bda2
Harsh Saini <harshsaini697@gmail.com>
2022-02-28 21:20:35 -0600

    N/A

--- solutions/1143. Longest Common Subsequence.java
+++ solutions/1143. Longest Common Subsequence.java
@@ -1,23 +1,18 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        if(text1 == null || text2 == null) {
+        return helper(text1, text2);
+    }
+    
+    private int helper(String t1, String t2) {
+        if(t1.length() == 0 || t2.length() == 0) {
             return 0;
         }
-        int len1 = text1.length();
-        int len2 = text2.length();
-        int result = 0;
-        int[][] dp = new int[len1 + 1][len2 + 1];
-        //dp[0][0] = 0;
-        for(int i = 1; i <= len1; i++) {
-            for(int j = 1; j <= len2; j++) {
-                if(text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                }
-                else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
+        int val = 0;
+        if(t1.charAt(0) == t2.charAt(0)) {
+            val = 1 + helper(t1.substring(1), t2.substring(1));
+        } else {
+            val = Math.max(helper(t1, t2.substring(1)), helper(t1.substring(1), t2));
         }
-        return dp[len1][len2];
+        return val;
     }
 }
