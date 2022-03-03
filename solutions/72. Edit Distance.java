db599651af2f336f288a2c464530c53204b81e75
Harsh Saini <harshsaini697@gmail.com>
2022-03-02 20:52:56 -0600

    N/A

--- solutions/72. Edit Distance.java
+++ solutions/72. Edit Distance.java
@@ -1,41 +1,29 @@
 class Solution {
-    public int minDistance(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
-        int[][] dp = new int[n + 1][m + 1];
-        if(m == 0) return n;
-        if(n == 0) return m;
-        for(int i = 0; i <= m; i++){
-            dp[0][i] = i;
+    public int minDistance(String w1, String w2) {
+        if(w1 == null || w2 == null) {
+            return 0;
         }
-        for(int i = 0; i <= n; i++){
-            dp[i][0] = i;
-        }
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= m; j++){
-                if(word1.charAt(i - 1) ==  word2.charAt(j - 1)){
-                    dp[i][j] = dp[i - 1][j - 1];
-                }else{
-                    dp[i][j] = 1+  Math.min(dp[i - 1][j], Math.min(dp[i - 1][ j - 1], dp[i][j - 1]));
-                }
-            }
-        }
-        return dp[n][m];
-        // return helper(word1, word2, 0, 0);
-    }
-    private int helper(String word1, String word2, int i , int j){
+        return helper(w1, w2, 0, 0);
         
-        if(j == word2.length()) return word1.length() - i;
-        if(i == word1.length()) return word2.length() - j; // no of letters left
-        int ans;
-        ///
-        if(word1.charAt(i) != word2.charAt(j)){
-            int insert = helper(word1, word2, i, j + 1);
-            int delete = helper(word1, word2, i + 1, j );
-            int replace = helper(word1, word2, i + 1, j + 1);
+    }
+    
+    private int helper(String w1, String w2, int i, int j) {
+        // edge cases
+        if(w1.length() == i) {
+            return w2.length() - j;
+        }
+        if(w2.length() == j) {
+            return w1.length() - i;
+        }
+        
+        int ans = 0;
+        if(w1.charAt(i) != w2.charAt(j)) {
+            int insert = helper(w1, w2, i, j + 1);
+            int delete = helper(w1, w2, i + 1, j);
+            int replace = helper(w1, w2, i + 1, j + 1);
             ans = 1 + Math.min(insert, Math.min(delete, replace));
-        }else{
-            ans = helper(word1, word2, i + 1, j + 1);
+        } else {
+            ans += helper(w1, w2, i + 1, j + 1);
         }
         return ans;
     }
