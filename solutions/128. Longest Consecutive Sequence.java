71d09a39523726db7baaf6569ae37a3caf5da61c
Harsh Saini <harshsaini697@gmail.com>
2022-04-02 10:18:04 -0500

    13 ms

--- solutions/128. Longest Consecutive Sequence.java
+++ solutions/128. Longest Consecutive Sequence.java
@@ -1,22 +1,21 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
         if(nums == null || nums.length == 0) return 0;
-        Arrays.sort(nums); // [0,1,1,2]
+        
+        Arrays.sort(nums);
+        boolean cons = false;
         int longestStreak = 1;
         int streak = 1;
-        final int limit = nums.length;
-        
-        for(int i = 1; i < limit; i++){
-            if(nums[i] != nums[i-1]){//skip duplicates
-                if(nums[i] == nums[i - 1] + 1){
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] != nums[i - 1]) {
+                if(nums[i] == nums[i - 1] + 1) {
                     streak++;
-                }else{
                     longestStreak = Math.max(longestStreak, streak);
+                } else {
                     streak = 1;
                 }
             }
         }
-        return Math.max(longestStreak, streak);
+        return longestStreak;
     }
 }
-//[1,2,3,4,100,200]
