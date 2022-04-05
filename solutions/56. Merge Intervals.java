835c9b83caa5fccb3c2ec03220f2d3628c378fb4
Harsh Saini <harshsaini697@gmail.com>
2022-04-05 16:41:48 -0500

    11 ms

---
+++ solutions/56. Merge Intervals.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if(intervals == null || intervals.length < 2) return intervals;
+        
+        Arrays.sort(intervals, (a,b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        LinkedList<int[]> list = new LinkedList<>();
+        list.add(intervals[0]);
+        for(int i = 1; i < intervals.length; i++) {
+            int[] interval = intervals[i];
+            if (list.isEmpty() || list.getLast()[1] < interval[0]){
+                list.add(interval);
+            } else {
+                list.getLast()[1] = Math.max(interval[1], list.getLast()[1]);
+            }
+        }
+        
+        return list.toArray(new int[list.size()][]);
+    }
+}
