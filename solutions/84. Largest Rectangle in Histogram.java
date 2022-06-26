cf83019a51ee7931d50dbe83f148afb7e67edf76
Harsh Saini <harshsaini697@gmail.com>
2022-06-26 12:42:50 -0500

    169 ms

--- solutions/84. Largest Rectangle in Histogram.java
+++ solutions/84. Largest Rectangle in Histogram.java
@@ -1,33 +1,18 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int Larea = 0;
-        int Garea = 0;
-        
-        Stack<Integer> st = new Stack<>();
-        int i = 0;
-        while(i < heights.length){
-            if(st.isEmpty() || heights[st.peek()] <= heights[i]){
-                st.push(i);
-                i++;
-            }else{
-                int top = st.pop();
-                if(st.isEmpty()){
-                    Larea = heights[top] * i;
-                }else{
-                    Larea = heights[top] * (i - st.peek() - 1);
+        int ans = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i <= heights.length; i++) {
+            while (!stack.isEmpty() && (i == heights.length || heights[stack.peek()] > heights[i])) {
+                int prev = stack.pop();
+                if (stack.isEmpty()) {
+                    ans = Math.max(ans, heights[prev] * i);
+                } else {
+                    ans = Math.max(ans, heights[prev] * (i - stack.peek() - 1));
                 }
             }
-            Garea = Math.max(Garea, Larea);
+            stack.push(i);
         }
-        while(!st.isEmpty()){
-            int top = st.pop();
-            if(st.isEmpty()){
-                Larea = heights[top] * i;
-            }else{
-                Larea = heights[top] * (i - st.peek() - 1);
-            }
-            Garea = Math.max(Garea, Larea);
-        }
-        return Garea;
+        return ans;
     }
 }
