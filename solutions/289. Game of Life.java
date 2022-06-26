c325ae7f3ae9e68add6e1ea7a474cf16c24a061e
Harsh Saini <harshsaini697@gmail.com>
2022-06-25 19:52:16 -0500

    1 ms

--- solutions/289. Game of Life.java
+++ solutions/289. Game of Life.java
@@ -1,33 +1,31 @@
 class Solution {
-    int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0},{-1,-1},{1,1},{-1,1},{1,-1}};
     public void gameOfLife(int[][] board) {
         if(board == null || board.length == 0) return;
-        int n = board.length;
-        int m = board[0].length;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                int temp = board[i][j];
+        int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0},{-1,-1},{1,1},{-1,1},{1,-1}};
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
                 int count = 0;
-                for(int[] d : dirs){
-                    int x = i + d[0];
-                    int y = j + d[1];
-                    if(x >= 0 && y >= 0 && x < n && y < m && (board[x][y] == 2 || board[x][y] == 1 )){
+                for(int[] dir: dirs) {
+                    int x = i + dir[0];
+                    int y = j + dir[1];
+                    if(x >= 0 && y >= 0 && x < board.length && y < board[0].length && (board[x][y] == 1 || board[x][y] == 2)) {
                         count++;
                     }
-                }
-                if(board[i][j] == 1 && ( count < 2 || count > 3)){
-                    board[i][j] = 2;
                 } 
-                if(board[i][j] == 0 && count == 3){
+                if(board[i][j] == 1 && count < 2) {
+                    board[i][j] = 2;
+                } else if (board[i][j] == 1 && count > 3) {
+                    board[i][j] = 2;
+                } else if (board[i][j] == 0 && count == 3) {
                     board[i][j] = 3;
                 }
-            }
-        }
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                board[i][j] %= 2;        
-            }
+            }  
         }
         
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                board[i][j] %= 2;
+            }
+        }
     }
 }
