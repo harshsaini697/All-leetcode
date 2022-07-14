3d723cd076a9a48ca657c101b715d5f2f552b3bd
Harsh Saini <harshsaini697@gmail.com>
2022-07-13 20:51:04 -0500

    33 ms

--- solutions/528. Random Pick with Weight.java
+++ solutions/528. Random Pick with Weight.java
@@ -1,25 +1,25 @@
 class Solution {
-​
-    Random random;
+    Random rand;
     int[] wSums;
-    
     public Solution(int[] w) {
-        this.random = new Random();
-        for(int i=1; i<w.length; ++i)
-            w[i] += w[i-1];
-        this.wSums = w;
+        rand = new Random(w.length);
+        wSums = new int[w.length];
+        for(int i = 1; i < w.length; i++) {
+            w[i] += w[i - 1];
+        }
+        wSums = w;
     }
     
     public int pickIndex() {
         int len = wSums.length;
-        int idx = random.nextInt(wSums[len-1]) + 1;
+        int idx = rand.nextInt( wSums[len - 1] ) + 1;
         int left = 0, right = len - 1;
         // search position 
         while(left < right){
-            int mid = left + (right-left)/2;
-            if(wSums[mid] == idx)
+            int mid = left + (right - left) / 2;
+            if (wSums[mid] == idx)
                 return mid;
-            else if(wSums[mid] < idx)
+            else if (wSums[mid] < idx)
                 left = mid + 1;
             else
                 right = mid;
