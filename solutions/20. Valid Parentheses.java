6fd62b3864c1c944f5a8271677e658b5ffc2b41c
Harsh Saini <harshsaini697@gmail.com>
2022-07-13 23:26:00 -0500

    2 ms

--- solutions/20. Valid Parentheses.java
+++ solutions/20. Valid Parentheses.java
@@ -1,25 +1,26 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s == null || s.length() == 0) return false;
-        Stack<Character> st = new Stack<>();
-        Map<Character, Character> map = new HashMap<>();
-        map.put('}', '{');
-        map.put(')', '(');
-        map.put(']', '[');
-        int i = 0;
-        while(i < s.length()){
-            char temp = s.charAt(i);
-            if(!map.containsKey(temp)){
-                st.push(temp);
-            }else{
-                if(!st.isEmpty() && st.peek() == map.get(temp)){
-                    st.pop();
-                }else{
+        if(s == null || s.length() == 0) {
+            return false;
+        }
+        Map<Character, Character> bracketMap = new HashMap<>();
+        bracketMap.put('}', '{');
+        bracketMap.put(')', '(');
+        bracketMap.put(']', '[');
+        
+        Stack<Character> openingLiteralStack = new Stack();
+        
+        for(char c: s.toCharArray()) {
+            if(!bracketMap.containsKey(c)) {
+                openingLiteralStack.push(c);
+            } else {
+                if(!openingLiteralStack.isEmpty() && openingLiteralStack.peek() == bracketMap.get(c)) {
+                    openingLiteralStack.pop();
+                } else {
                     return false;
                 }
             }
-            i++;
         }
-        return st.size() == 0;
+        return openingLiteralStack.isEmpty();
     }
 }
